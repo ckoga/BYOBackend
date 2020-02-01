80348e0e9bfdf2ffda93f2a5c7290dffe817b82c
Colin Turing <colin.koga@gmail.com>
2020-02-01 08:36:42 -0700

    Add GET endpoint for all publishers

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 const express = require('express');
 const app = express();
+
+app.use(express.json())
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'BYOB';
@@ -8,6 +13,17 @@ app.get('/', (request, response) => {
   response.send('hello world')
 });
 
+app.get('/api/v1/publishers', async (request, response) => {
+  try {
+    const publishers = await database('publishers').select();
+    response.status(200).json(publishers)
+  } catch(error) {
+    response.status(500).json({ error })
+  }
+})
+
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+  console.log(
+    `${app.locals.title} is running on http://localhost:${app.get('port')}.`
+    );
 });
