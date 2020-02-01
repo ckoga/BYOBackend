004e977c21534448e85ecc1540ff68c60aa8c70e
Colin Turing <colin.koga@gmail.com>
2020-02-01 09:20:07 -0700

    Add POST endpoint for publishers

--- server.js
+++ server.js
@@ -39,6 +39,26 @@ app.get('/api/v1/publishers/:id', async (request, response) => {
   }
 });
 
+app.post('/api/v1/publishers', async (request, response) => {
+  const publisher = request.body;
+
+  for (let requiredParameter of ['publisher', 'location']) {
+    if (!publisher[requiredParameter]) {
+      return response.status(422).send({
+        error: `expected POST format: { publisher: <string>, location: <string> } 
+        . You're missing the ${requiredParameter} property`
+      })
+    }
+  }
+
+  try {
+    const id = await database('publishers').insert(publisher, 'id')
+    response.status(201).json(id[0])
+  } catch(error) {
+    response.status(500).json({ error })
+  }
+})
+
 app.listen(app.get('port'), () => {
   console.log(
     `${app.locals.title} is running on http://localhost:${app.get('port')}.`
