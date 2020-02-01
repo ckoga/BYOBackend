ceab86f8b8be57580a3ae0ae3cdaf83807a01fd1
Colin Turing <colin.koga@gmail.com>
2020-02-01 15:25:06 -0700

    Add DELETE endpoint for a publisher given by id

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/api/v1/publishers', async (request, response) => {
   }
 });
 
-app.get('/api/v1/publishers/:id', async (request, response) => { // would like to get the mangas from the manga table as welll, look into "linking dynmics routes (??)"
+app.get('/api/v1/publishers/:id', async (request, response) => { 
   try {
     const publisher = await database('publishers')
       .where('id', request.params.id)
@@ -39,6 +39,27 @@ app.get('/api/v1/publishers/:id', async (request, response) => { // would like t
   }
 });
 
+// app.get('/api/v1/publishers/:id/mangas', async (request, response) => { // would like to get the mangas from the manga table as welll, look into "linking dynmics routes (??)"
+//   try {
+//     const publisher = await database('publishers')
+//       .where('id', request.params.id)
+//       .select()
+//     const mangas = await database('mangas')
+//       .where('publisher_id', request.params.id)
+//       .select()
+  
+//     if (publisher.length) {
+//       response.status(200).json({publisher: publisher, mangas: manga})
+//     } else {
+//       response.status(404).json({
+//         error: `Could not find locations with id of ${required.params.id}`
+//       })
+//     }
+//   } catch (error) {
+//     response.status(500).json({ error })
+//   }
+// });
+
 app.post('/api/v1/publishers', async (request, response) => {
   const publisher = request.body;
 
@@ -53,7 +74,7 @@ app.post('/api/v1/publishers', async (request, response) => {
 
   try {
     const id = await database('publishers').insert(publisher, 'id')
-    response.status(201).json(id[0])
+    response.status(201).json({ publisher_id: id[0], publisher: publisher })
   } catch(error) {
     response.status(500).json({ error })
   }
@@ -77,10 +98,27 @@ app.get('/api/v1/mangas/:id', async (request, response) => {
       response.status(200).json(manga)
     } else {
       response.status(404).json({
-        error: `Could not fine the manga with id of ${request.params.id}.`
+        error: `Could not find the manga with id of ${request.params.id}.`
       });
     } 
   } catch (error) {
+    response.status(500).json({ error })
+  }
+});
+
+app.delete('/api/v1/publishers/:id', async (request, response) => {
+  const { id } = request.params
+
+  if (!id) {
+    response.status(422).send({ error: "Invalid publisher id check url"})
+  }
+
+  try {
+    await database('publishers')
+      .where('id', id)
+      .del()
+    response.status(204).send()
+  } catch(error) {
     response.status(500).json({ error })
   }
 })
